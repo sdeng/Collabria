3dc840a7bc9c2a91b4f3f1618adcff3e80e640bf
Sam Deng <SamDeng@Gmail.com>
2012-10-01 02:28:06 -0400

    Saving human readable timestamp as well as UTC timstamp.

--- collaboria.js
+++ collaboria.js
@@ -4,15 +4,21 @@ var messages = new Meteor.Collection('messages');
 if (Meteor.isClient) {
     // Populate messages in chat template
     Template.chat.messages = function() {
-        return messages.find({}, {sort: {timestamp: -1}});
+        return messages.find({}, {sort: {timestamp_utc: -1}});
     }
 
     // Submit message with enter key
     $('#input-box').live('keydown', function(event) {
-        var timestamp = Date.now();
+        var timestamp, timestamp_utc;
         var message = $('#input-box')[0].value;
         if ((event.keyCode == 13) && (message.length > 0)) {
-            messages.insert({message: message, timestamp: timestamp});
+            timestamp_utc = Date.now();
+            timestamp = Date(timestamp_utc).split(' ')[4];
+            messages.insert({
+                message: message,
+                timestamp_utc: timestamp_utc,
+                timestamp: timestamp
+            });
             $('#input-box')[0].value = '';
         }
     });
