0320d9ae67c2212f6b5ba01b0f0bd070eea4b52c
Sam Deng <SamDeng@Gmail.com>
2012-10-07 18:41:13 -0400

    Added colors to identify differnet users, in lieu of a real authentication system.

--- collabria.js
+++ collabria.js
@@ -2,6 +2,12 @@ var messages = new Meteor.Collection('messages');
 
 
 if (Meteor.isClient) {
+    var color = '#';
+    var letters = '0123456789ABCDEF'.split('');
+    for (var i=0; i<6; i++) {
+        color += letters[Math.round(Math.random()*15)];
+    }
+
     // Populate messages in chat template
     Template.chat.messages = function() {
         return messages.find({}, {sort: {timestamp_utc: -1}});
@@ -12,7 +18,7 @@ if (Meteor.isClient) {
         var message = $('#input-box')[0].value;
         if ((event.keyCode == 13) && (message.length > 0)) {
             $('#input-box')[0].value = '';
-            Meteor.call('insert_message', message);
+            Meteor.call('insert_message', color, message);
         }
     });
 }
@@ -24,11 +30,12 @@ if (Meteor.isServer) {
     });
 
     Meteor.methods({
-        insert_message: function(message) {
+        insert_message: function(color, message) {
             var timestamp, timestamp_utc;
             timestamp_utc = Date.now();
             timestamp = Date(timestamp_utc).split(' ')[4];
             messages.insert({
+                color: color,
                 message: message,
                 timestamp_utc: timestamp_utc,
                 timestamp: timestamp
